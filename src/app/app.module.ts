cacb1793a5adb0ea3cf67dca577daae73c772cee
JeremyMcdr <jeremymacadre@gmail.com>
2023-04-21 09:57:24 +0200

    sd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { TopHeroesComponent } from './top-heroes/top-heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroesListComponent,
+    TopHeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: 'heroes-list', component: HeroesListComponent },
+      { path: 'heroes-top', component: TopHeroesComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes.ts
@@ -0,0 +1,44 @@
+export interface Product {
+  id: number;
+  heroName: string;
+}
+
+export const heroes = [
+  {
+    id: 1,
+    heroName: 'Iron Man',
+    isTop:1,
+  },
+  {
+    id: 2,
+    heroName: 'Super Man',
+    isTop:1,
+  },
+  {
+    id: 3,
+    heroName: 'Gros Toinou',
+    isTop:0,
+  },
+  {
+    id: 4,
+    heroName: 'Wonder Woman',
+    isTop:0,
+  },
+  {
+    id: 5,
+    heroName: 'KERC',
+    isTop:0,
+  },
+  {
+    id: 6,
+    heroName: 'ZIAK',
+    isTop:0,
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
